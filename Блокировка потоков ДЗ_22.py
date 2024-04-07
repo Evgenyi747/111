5fccf577a15ec7452eb8585c27fb58293018d403
Evgenyi747 <155776816+Evgenyi747@users.noreply.github.com>
2024-04-07 12:17:32 +0300

    Add files via upload

---
+++ Блокировка потоков ДЗ_22.py
@@ -0,0 +1,60 @@
+import random
+from collections import defaultdict
+import threading
+from threading import Thread
+
+
+class BankAccount (Thread):
+
+    def __init__(self, account, amount, *args, **kwargs):
+        super().__init__()
+        self.account = account
+        self.amount = amount
+        self.deposit = deposit
+        self.withdraw = withdraw
+
+
+def deposit_task(account, amount):
+    for _ in range(5):
+        account.deposit(amount)
+        account = BankAccount()
+
+        global a, b
+        print(f'{_}: deposit_task wait lock_A', flush=True)
+        with lock_A:
+            print(f'{_}: deposit_task take lock_A', flush=True)
+            a += 1
+            print(f'{_}: deposit_task wait lock_B', flush=True)
+            with lock_B:
+                print(f'{_}: deposit_task take lock_B', flush=True)
+                b -= 1
+
+def withdraw_task(account, amount):
+    for _ in range(5):
+        account.withdraw(amount)
+        account = BankAccount()
+
+        global a, b
+        print(f'{_}: withdraw_task wait lock_B', flush=True)
+        with lock_A:
+            print(f'{_}: withdraw_task take lock_B', flush=True)
+            b -= 1
+            print(f'{_}: withdraw_task wait lock_A', flush=True)
+            with lock_B:
+                print(f'{_}: withdraw_task take lock_A', flush=True)
+                a += 1
+
+a = 100
+b = 150
+lock_A = threading.RLock()
+lock_B = threading.RLock()
+account = 5
+
+deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
+withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
+
+deposit_thread.start()
+withdraw_thread.start()
+
+deposit_thread.join()
+withdraw_thread.join()
